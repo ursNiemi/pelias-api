51e36419d2cb14de7f6ae2e9568c30a64105ced8
Joonas Rissanen <joonas.rissanen@cgi.com>
2019-08-26 14:50:26 +0300

    fix

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 #workaround for azure DNS issue
 
-# if [ "$EUID" -eq 0 ]
-#   then echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
-# fi
+if [ "$EUID" -eq 0 ]
+  then echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
+fi
 
 #start pelias api
 npm start
